7173c35156fb0e1a42b0408eda57333d4ac44df5
Artem Kazimirov <nexus.artem@gmail.com>
2022-09-05 00:18:42 +0300

    add api connect

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -2,26 +2,28 @@ import React from 'react'
 
 import styles from './Games.module.scss'
 
-const Games = () => {
+const Games = ({ games }) => {
   return (
     <div className={styles.main}>
-      <div className={styles.game}>
-        <img
-          className={styles.picture}
-          src="https://media.rawg.io/media/resize/640/-/screenshots/3a6/3a6ee3b1637d05b0e48db56ffaf62ade.jpg"
-          alt="game"
-        />
-        <div className={styles.info}>
+      {games.map(game => (
+        <div className={styles.game}>
           <img
-            className={styles.platform}
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Windows_logo_-_2012.svg/1280px-Windows_logo_-_2012.svg.png"
-            alt="platform_icon"
+            className={styles.picture}
+            src={game?.background_image}
+            alt={game?.name}
           />
-          <a href="#" className={styles.name}>Obligue Of CHAOS</a>
-          <span >Release date: </span>
-          <span>Rating: </span>
+          <div className={styles.info}>
+            <img
+              className={styles.platform}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Windows_logo_-_2012.svg/1280px-Windows_logo_-_2012.svg.png"
+              alt="platform_icon"
+            />
+            <a href="#" className={styles.name}>{game?.name}</a>
+            <span >Release date: {game?.released}</span>
+            <span>Rating: {game?.metacritic}</span>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
